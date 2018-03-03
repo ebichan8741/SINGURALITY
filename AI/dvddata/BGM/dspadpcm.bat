c0e4d167515fd7bb5341c7cb7a5c402ab0900b3e
ebichan8741 <31872382+ebichan8741@users.noreply.github.com>
2018-03-03 17:52:51 +0900

    Add files via upload

---
+++ AI/dvddata/BGM/dspadpcm.bat
@@ -0,0 +1,4 @@
+C:\RVL_SDK\X86\bin\dspadpcm -E gameBGM_l.wav
+C:\RVL_SDK\X86\bin\dspadpcm -E gameBGM_r.wav
+pause
+
---
+++ AI/dvddata/SE/AI_se.h
@@ -0,0 +1,32 @@
+//  ********************
+//  SE_SUMMON
+//  ********************
+#define SE_SUMMON		0
+//  ********************
+//  SE_ATTACK_SHOT
+//  ********************
+#define SE_ATTACK_SHOT		1
+//  ********************
+//  SE_DEFENCE_GUARD
+//  ********************
+#define SE_DEFENCE_GUARD		2
+//  ********************
+//  SE_SPEED_SHOT
+//  ********************
+#define SE_SPEED_SHOT		3
+//  ********************
+//  SE_AI_LOCKON
+//  ********************
+#define SE_AI_LOCKON		4
+//  ********************
+//  SE_MANA_CHARGE
+//  ********************
+#define SE_MANA_CHARGE		5
+//  ********************
+//  SE_LEVEL_UP
+//  ********************
+#define SE_LEVEL_UP		6
+//  ********************
+//  SE_LEVEL_DOWN
+//  ********************
+#define SE_LEVEL_DOWN		7
